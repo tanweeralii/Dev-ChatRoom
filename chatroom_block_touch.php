cc0f353aa325d4ab718241748a026bcac2021036
TANWEER ALI <tanweeralii908@gmail.com>
2020-03-11 00:53:54 +0530

    Update chatroom_block_touch.php

--- chatroom_block_touch.php
+++ chatroom_block_touch.php
@@ -1,17 +1,11 @@
 <?php
-$servername = "";
-$username = "";
-$password = "";
-$dbname =  "";
-$conn = pg_connect($servername, $username, $password, $dbname);
-$sql = "SELECT user1 FROM block WHERE user2='".$_POST['user2']."'";
-$result = pg_query($conn, $sql);
+$db = pg_connect("host= dbname= user= password=");
+$result = pg_query($db, "SELECT user1 FROM block WHERE user2='".$_POST['user2']."'");
 $result1=pg_fetch_assoc($result);
 if($result1['user1']==$_POST['user1']){
 	echo "1";
 }
-$sql2 = "SELECT user1 FROM block WHERE user2='".$_POST['user1']."'";
-$result2 = pg_query($conn, $sql2);
+$result2 = pg_query($db, "SELECT user1 FROM block WHERE user2='".$_POST['user1']."'");
 $result3 = pg_fetch_assoc($result2);
 if($result3['user1']==$_POST['user2']){
 	echo "2";
