88948d5e20e62f08e08f8518451592a178ea8ce1
TANWEER ALI <tanweeralii908@gmail.com>
2020-03-11 00:49:18 +0530

    Update chatroom_insert.php

--- chatroom_insert.php
+++ chatroom_insert.php
@@ -3,14 +3,10 @@ $timezone = date_default_timezone_set('Asia/Calcutta');
 $date = date('Y-m-d', time());
 $time = date('H:i:s', time());
 
-$servername = "";
-$username = "";
-$password = "";
-$dbname =  "";
-$conn = pg_connect($servername, $username, $password, $dbname);
+$db = pg_connect("host= dbname= user= password=");
 
-$result = pg_query($conn, "INSERT INTO conversation(sender, receiver, date1, os, time1, ip) VALUES('".$_POST['sender']."', '".$_POST['receiver']."', '$date', '".$_POST['os']."', '$time', '".$_POST['ip']."')");
+$result = pg_query($db, "INSERT INTO conversation(sender, receiver, date1, os, time1, ip, type) VALUES('".$_POST['sender']."', '".$_POST['receiver']."', '$date', '".$_POST['os']."', '$time', '".$_POST['ip']."', 'text')");
 
-$result1 = pg_query($conn, "INSERT INTO reply(chat_id, message) VALUES((SELECT chat_id FROM conversation WHERE date1='$date' AND time1='$time'), '".$_POST['text']."')");
+$result1 = pg_query($db, "INSERT INTO reply(chat_id, message) VALUES((SELECT chat_id FROM conversation WHERE date1='$date' AND time1='$time'), '".$_POST['text']."')");
 
 ?>
