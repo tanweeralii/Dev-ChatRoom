e3d0cc24bc9e2c9e30f44e86c2698ae467a7b871
TANWEER ALI <tanweeralii908@gmail.com>
2020-03-14 01:03:21 +0530

    Update main.sql

--- main.sql
+++ main.sql
@@ -9,4 +9,4 @@ ALTER SEQUENCE reply_reply_id_seq RESTART WITH 20001;
                                                                                                                                       
 CREATE TABLE block(user1 VARCHAR(30), user2 VARCHAR(30));
 CREATE TABLE chatroom_credentials(id1 tinyint(1), ID smallint(6) PRIMARY KEY, first_name VARCHAR(265) NOT NULL, last_name VARCHAR(256), email VARCHAR(256) NOT NULL, status tinyint(1), password VARCHAR(256), uname VARCHAR(30)); 
-                                                                                                                          
+ALTER SEQUENCE chatroom_credentials_ID_seq RESTART WITH 10001;                                                                                                                     
